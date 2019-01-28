2f6edde0262d5bea7205564b6c45ce643df6d874
Johnathon Wood <jaywood128@gmail.com>
2019-01-28 22:58:53 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     Song.all do |song|
     binding.pry
+  end
     # return an array of strings containing every musician's name
   end
 end
