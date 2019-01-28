f64c062825fd6ac88cdb2ecb944fc94a42df92cc
Johnathon Wood <jaywood128@gmail.com>
2019-01-28 22:22:13 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,8 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    binding.pry
-    Artist.first.genres
+    self.songs
   end
 
   def song_count
